93deb7f87a25ff1c8e1b2b79b51dce1d86d28c8b
Stonehagen <steinhagen@mailbox.org>
2023-01-06 23:36:32 +0100

    add function for generate simple page layout

--- src/index.js
+++ src/index.js
@@ -5,11 +5,31 @@ const contentContainer = document.querySelector('#content');
 
 const component = () => {
   const element = document.createElement('div');
+  const textWrapper = document.createElement('div');
+
+  const heading = document.createElement('h2');
+  heading.innerHTML = 'The Dinning Room';
+  textWrapper.appendChild(heading);
+
+  const text = document.createElement('p');
+  text.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
+    est laborum.`;
+
+  textWrapper.appendChild(text);
+  textWrapper.classList.add('main-wrapper');
+
+  element.appendChild(textWrapper);
 
   const restaurantImage = new Image();
   restaurantImage.src = RestaurantImage;
-
   element.appendChild(restaurantImage);
+
+  element.classList.add('main');
 
   return element;
 };
