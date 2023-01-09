bbe82e5db750fdd047fca2eee25b7e812e89c44d
Stonehagen <steinhagen@mailbox.org>
2023-01-09 21:30:55 +0100

    add card layout for menu items

--- src/modules/menu-page.js
+++ src/modules/menu-page.js
@@ -1,39 +1,104 @@
-import RestaurantImage from '../images/drink.jpg';
-
 export default () => {
   const element = document.createElement('div');
-  const textWrapper = document.createElement('div');
+  element.classList.add('main-food');
 
-  const divider = document.createElement('h1');
-  divider.innerHTML = 'Beverages';
-  divider.classList.add('divider');
-  element.appendChild(divider);
+  const menu = {
+    Beverages: [
+      {
+        name: 'Red Bubble Water',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing 
+      elit, sed do eiusmod tempor incididunt ut labore et dolore`,
+        price: '5',
+      },
+      {
+        name: 'Cheap Soda',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing 
+      elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam`,
+        price: '4',
+      },
+      {
+        name: 'Some Fluid',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing 
+      elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad`,
+        price: '7.5',
+      },
+      {
+        name: 'Green Hoax',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing 
+      elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim`,
+        price: '8',
+      },
+      {
+        name: 'Soda With Nothing',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing 
+      elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam`,
+        price: '5.5',
+      },
+    ],
+    Lunch: [
+      {
+        name: 'Red Stuffed Burger',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing 
+      elit, sed do eiusmod tempor`,
+        price: '14',
+      },
+      {
+        name: 'Orange Bowl',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing 
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam`,
+        price: '16.5',
+      },
+      {
+        name: 'Black Salad',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing 
+      elit, sed do eiusmod tempor`,
+        price: '16',
+      },
+      {
+        name: 'Just Ginger',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing 
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad`,
+        price: '54.5',
+      },
+    ],
+  };
 
-  const heading = document.createElement('h2');
-  heading.innerHTML = 'Some Tasty Drink';
-  textWrapper.appendChild(heading);
+  Object.keys(menu).forEach((category) => {
+    const divider = document.createElement('h1');
+    divider.innerHTML = category;
+    divider.classList.add('divider');
+    element.appendChild(divider);
 
-  const text = document.createElement('p');
-  text.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing 
-  elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-  voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-  occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-  anim id est laborum.`;
+    menu[category].forEach((item) => {
+      const textWrapper = document.createElement('div');
+      textWrapper.classList.add('text-wrapper');
 
-  textWrapper.appendChild(text);
+      const heading = document.createElement('h2');
+      heading.innerHTML = item.name;
+      textWrapper.appendChild(heading);
 
-  textWrapper.classList.add('main-wrapper');
+      const text = document.createElement('p');
+      text.innerHTML = item.text;
+      textWrapper.appendChild(text);
 
-  element.appendChild(textWrapper);
+      const price = document.createElement('p');
+      price.innerHTML = item.price;
+      price.classList.add('price-tag');
+      textWrapper.appendChild(price);
 
-  const restaurantImage = new Image();
-  restaurantImage.src = RestaurantImage;
-  restaurantImage.classList.add('food-img');
-  element.appendChild(restaurantImage);
+      const itemCard = document.createElement('div');
+      itemCard.classList.add('menu-card');
+      itemCard.appendChild(textWrapper);
 
-  element.classList.add('main');
+      element.appendChild(itemCard);
+    });
+  });
 
   return element;
 };
